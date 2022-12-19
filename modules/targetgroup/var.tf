1c9267204a07ba50adffe27a6ed632193a53e7ee
Mohammed Harris <96809941+Harris2711@users.noreply.github.com>
2022-12-19 17:57:26 +0530

    Update var.tf

--- modules/targetgroup/var.tf
+++ modules/targetgroup/var.tf
@@ -1,11 +1,11 @@
 variable "instance_id" {
-    default="i-0274536fd4a625e39"
+    default="menton your instance id"
 }
 
 variable "vpc_id" {
-    default="vpc-001790fa349af734c"
+    default="mention vpc id"
 }
 
 variable "arn" {
-    default="arn:aws:elasticloadbalancing:ap-south-1:310486407552:targetgroup/lb-tg/c487fd1d8301f404"
+    default="mention target group arn"
 }
