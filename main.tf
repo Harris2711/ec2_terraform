62193e791ca04199e122456f1d372e08bfcf6cfd
Mohammed Harris <96809941+Harris2711@users.noreply.github.com>
2022-12-19 18:00:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 module "instance" {
     source                = "./modules/EC2/"
-    ami_vpc               ="ami-08ee6644906ff4d6c"
-    instance_type         ="t2.micro"
-    vpc_id                ="vpc-001790fa349af734c"
-    subnet_id             ="subnet-078387f27e885f97f"
+    ami_vpc               ="mention ami id"
+    instance_type         ="mention instance id"
+    vpc_id                ="mention vpc id "
+    subnet_id             ="mention subnet-id"
 }
 
 # module "loadbalancer" {
